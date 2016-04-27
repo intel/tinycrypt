cc85eb7cf897ae694693dcd775829edd9bce1656
Florin Iucha <florin@iucha.net>
2016-04-27 00:13:00 -0400

    Makefile updates for MinGW

--- config.mk
+++ config.mk
@@ -6,12 +6,16 @@
 #
 ################################################################################
 
-CC=gcc 
-CFLAGS = -Os -std=c99 -Wall -Wextra -I../lib/include/ -I../lib/source/ -I../tests/include/
-VPATH = ../lib/include/tinycrypt/ ../lib/source/
+CC:=gcc
+CFLAGS:=-Os -std=c99 -Wall -Wextra -D_ISOC99_SOURCE -MMD -I../lib/include/ -I../lib/source/ -I../tests/include/
+vpath %.c ../lib/source/
 
-export CC
-export CFLAGS
-export VPATH
+# override MinGW built-in recipe
+%.o: %.c
+	$(COMPILE.c) $(OUTPUT_OPTION) $<
+
+ifeq ($(OS),Windows_NT)
+DOTEXE:=.exe
+endif
 
 ################################################################################
