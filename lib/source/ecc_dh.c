3db4aa9644896162ecbf966b9597ea5f45ac6538
Rafael Misoczki <rafa.misoczki@gmail.com>
2019-09-10 17:58:11 -0400

    enabling side-channel countermeasure for EC-DH

--- lib/source/ecc_dh.c
+++ lib/source/ecc_dh.c
@@ -60,12 +60,6 @@
 #include <tinycrypt/utils.h>
 #include <string.h>
 
-#if default_RNG_defined
-static uECC_RNG_Function g_rng_function = &default_CSPRNG;
-#else
-static uECC_RNG_Function g_rng_function = 0;
-#endif
-
 int uECC_make_key_with_d(uint8_t *public_key, uint8_t *private_key,
 			 unsigned int *d, uECC_Curve curve)
 {
@@ -174,7 +168,7 @@ int uECC_shared_secret(const uint8_t *public_key, const uint8_t *private_key,
 
 	/* If an RNG function was specified, try to get a random initial Z value to
 	 * improve protection against side-channel attacks. */
-	if (g_rng_function) {
+	if (uECC_get_rng()) {
 		if (!uECC_generate_random_int(p2[carry], curve->p, num_words)) {
 			r = 0;
 			goto clear_and_out;
