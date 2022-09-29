98951899c0e2f40f35f04b4a0bfc330f6b9290c8
okorach <olivier.korach@gmail.com>
2022-09-29 09:00:47 +0200

    Added better log management

--- tis.mk
+++ tis.mk
@@ -3,9 +3,10 @@ TEST_SRC = $(wildcard tests/test_*.c)
 PROD_SRC = $(wildcard lib/source/*.c)
 TEST_LOG = $(TEST_SRC:.c=.c.tis.log)
 TIS_REPORT =
-
-TARGET = tis.log
-ISSUES_FILE = tis-issues.log
+LOGS = logs
+RESULTS = _results
+TARGET = $(LOGS)/tis.log
+ISSUES_FILE = $(LOGS)/tis-issues.log
 
 include colors.mk
 include inc.mk
@@ -13,27 +14,29 @@ include inc.mk
 .PHONY: tis_preamble tis_report_preamble tis clean
 
 clean:
-	rm -f $(TARGET) $(ISSUES_FILE) $(TEST_LOG)
+	rm -rf $(TARGET) $(ISSUES_FILE) $(LOGS) $(RESULTS)
 
 %.c.tis.log: %.c
-	@echo "" | tee $<.tis.log
-	@echo "Date:" `date`  | tee -a $<.tis.log
+	$(eval SHORT := $(shell echo $< | sed 's/.*\/test_//'))
+	@echo "" | tee $(LOGS)/$(SHORT).tis.log
+	@echo "Date:" `date`  | tee -a $(LOGS)/$(SHORT).tis.log
 	@echo "Running $(FONT_BOLD)tis-analyzer$(FONT_RESET) in batch mode"
-	@echo "$(FONT_GREEN)tis-analyzer -tis-config-load tis-config.json -slevel 100 -val -I./lib/include -Itests/include -D_TRUST_tests/test_aes.c lib/source/aes_decrypt.c lib/source/aes_encrypt.c$(FONT_RESET)" | tee -a $<.tis.log
-	@tis-analyzer -tis-config-load tis-config.json -slevel 100 -val -I./lib/include -I./tests/include $<  $(TIS_REPORT)  $(PROD_SRC) | tee -a $<.tis.log
+	@echo "$(FONT_GREEN)tis-analyzer -tis-config-load tis-config.json -slevel 100 -val -I./lib/include -I./tests/include $< $(PROD_SRC) -tis-report -info-json-results _results/$(SHORT).results.json -info-csv-all _results/$(SHORT).res$(FONT_RESET)" | tee -a $(LOGS)/$(SHORT).tis.log
+	@tis-analyzer -slevel 100 -val -I./lib/include -I./tests/include $< $(PROD_SRC) -tis-report -info-json-results _results/$(SHORT).results.json -info-csv-all _results/$(SHORT).res | tee -a $(LOGS)/$(SHORT).tis.log
+	@cat $(LOGS)/$(SHORT).tis.log >> $(TARGET)
 
 tis_preamble:
 	@clear
+	@mkdir -p $(LOGS) $(RESULTS)
 	@echo "" | tee -a $(TARGET)
 	@echo "Date:" `date`  | tee -a $(TARGET)
 	@echo "Running $(FONT_BOLD)tis-analyzer$(FONT_RESET) in cmd line mode on all tests"
 
 tis: tis_preamble $(TEST_LOG)
-	# Consolidate all logs in one, then extract and count warnings
-	cat $(TEST_LOG) >$(TARGET)
-	@grep ":\[kernel\] warning" $(TARGET) | grep -v -E "no side-effect|Calling undeclared function" | sort | uniq -c | tee $(ISSUES_FILE)
+	# Extract and count warnings
+	@grep -E ":\[(kernel|value|sa)\] warning|              " $(TARGET) | grep -v -E "no side-effect|Calling undeclared function| Ignoring." | tee $(ISSUES_FILE)
 	@echo "========================================="
-	@echo "     " `wc -l < $(ISSUES_FILE)` UNDEFINED BEHAVIOR ISSUES
+	@echo "     " `grep ":\[kernel\] warning" $(ISSUES_FILE) | sort | uniq -c | wc -l` UNDEFINED BEHAVIOR ISSUES
 	@echo "========================================="
 
 	# Test if issues file contains warning, and exit with failure code if so
@@ -43,5 +46,8 @@ tis_report_preamble:
 	TIS_REPORT = "-tis-report"
 
 tis_report: tis_preamble $(TEST_LOG)
+	@echo "$(FONT_CYAN)Generating HTML analysis report$(FONT_RESET)"
+	@echo "$(FONT_GREEN)tis_report _results/$(FONT_RESET)"
+	@tis-report _results/
 
 
