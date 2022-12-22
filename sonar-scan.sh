7ecd6f3b67ab97f2877cf806357393df191e1502
okorach <olivier.korach@gmail.com>
2022-12-22 09:38:33 +0100

    Build before scan

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -2,5 +2,17 @@
 
 set -euo pipefail
 
+BW_OUT=bw-out
+
+make clean
+
+build-wrapper-linux-x86-64 --out-dir $BW_OUT make
+# bear make
+# sonar.cfamily.compile-commands=compile_commands.json
+
 VERSION=$(date -Iseconds)
-~/sonar/scanner-4.7/bin/sonar-scanner -Dsonar.projectVersion="$VERSION" $*
+
+~/sonar/scanner-4.7/bin/sonar-scanner \
+   -Dsonar.projectVersion="$VERSION" \
+   -Dsonar.cfamily.build-wrapper-output=$BW_OUT \
+   $*
